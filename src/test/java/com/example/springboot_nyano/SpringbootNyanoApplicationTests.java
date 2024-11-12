b5dcae5196f95b858d402760693d33033119d718
anbui <buiphuan021100@gmail.com>
2024-11-13 01:38:16 +0700

    add source

---
+++ src/test/java/com/example/springboot_nyano/SpringbootNyanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_nyano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNyanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
