8dbc3ed7bcc74b5ccbf97e6a2e001b5a43f65be0
anbui <buiphuan021100@gmail.com>
2024-11-14 16:36:23 +0700

    Java 08: Pageable with spring JPA

--- src/main/java/com/andev/controller/UserCURDController.java
+++ src/main/java/com/andev/controller/UserCURDController.java
@@ -3,6 +3,10 @@ package com.andev.controller;
 import com.andev.entity.user.UserEntity;
 import com.andev.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +20,35 @@ public class UserCURDController {
         return userService.createUser(userEntity);
     }
 
-@GetMapping("/search")
+    @GetMapping("/search")
     public UserEntity searchUser (@RequestParam String name, @RequestParam String email) {
         return  userService.findByUserNameAndUserEmail(name, email);
+    }
+
+    @GetMapping("/getAll")
+    public Page<UserEntity> getAll (
+            @RequestParam int page,
+            @RequestParam int size,
+            @RequestParam(defaultValue = "id") String sort,
+            @RequestParam(defaultValue = "desc") String direction
+    ) {
+        Sort.Direction sortDirection =  direction.equalsIgnoreCase("asc") ? Sort.Direction.ASC: Sort.Direction.DESC;
+        Sort sortBy = Sort.by(sortDirection, sort);
+        Pageable pageable = PageRequest.of(page, size, sortBy);
+        return userService.findAllUsers(pageable);
+    }
+
+    @GetMapping("/searchPage")
+    public Page<UserEntity> searchPageUserName (
+            @RequestParam String name,
+            @RequestParam int page,
+            @RequestParam int size,
+            @RequestParam(defaultValue = "id") String sort,
+            @RequestParam(defaultValue = "desc") String direction
+    ) {
+        Sort.Direction sortDirection =  direction.equalsIgnoreCase("asc") ? Sort.Direction.ASC: Sort.Direction.DESC;
+        Sort sortBy = Sort.by(sortDirection, sort);
+        Pageable pageable = PageRequest.of(page, size, sortBy);
+        return  userService.findByUserName(name, pageable);
     }
 }
